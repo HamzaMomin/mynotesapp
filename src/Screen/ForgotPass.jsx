c4042d345891fd4c72f30f8e1088127fa733a106
Hamza Momin <65024078+HamzaMomin@users.noreply.github.com>
2024-01-18 15:58:33 +0500

    changes

--- src/Screen/ForgotPass.jsx
+++ src/Screen/ForgotPass.jsx
@@ -3,7 +3,23 @@ import React from 'react';
 const ForgotPassword = () => {
   return (
     <div>
-     
+       <div className="box">
+            <form>
+                
+                <div className="input-container">
+                    <input type="text" required />
+                    <label>Email</label>
+                </div>
+                <div className="input-container">
+                    <input type="Password" required="" />
+                    <label>Password</label>
+                </div>
+                
+                <button type="button" className="btn">Login</button>
+              
+                
+            </form>
+        </div>
       
     </div>
   );
